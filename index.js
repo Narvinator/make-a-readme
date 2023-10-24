bf0aaa69fa2489458e506ff183c9a16d607decd5
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-10-24 01:25:22 -0400

    started on questions and tested inquirer

--- index.js
+++ index.js
@@ -1,14 +1,69 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown.js");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "name",
+    message: "Enter your first and last name",
+    validate: nameInput => {
+        if (nameImput) {
+            return true;
+        } else {
+            console.log("Enter your last and first name");
+            return false;
+        }
+    }
+},
+{
+    type: "input",
+    name: "gitHub",
+    message: "Enter your GitHub username",
+    validate: githubInput => {
+        if (githubInput) {
+            return true;
+        } else {
+            console.log("Enter your GitHub account");
+            return false
+        }
+    }
+},
+{
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+    validate: emailInput => {
+        if (emailInput) {
+            return true;
+        } else {
+            console.log("Provide your email address:");
+            return false;
+        }
+    }
+},
+{
+    type: "input",
+    name: "description",
+    message: "Enter your description for your project",
+    validate: descripionInput => {
+        if (descriptionInput) {
+            return true;
+        } else {
+            console.log("Enter your project description");
+            return false;
+        }
+    }
+}  
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
